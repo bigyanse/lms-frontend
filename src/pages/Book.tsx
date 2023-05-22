dce1ebae95971df2e7d43b78a55aa50c7c21e390
bigyanse <contact.bigyanse@gmail.com>
2023-05-22 18:25:16 +0545

    `update`: fix options when user is not logged in to edit and delete book

--- src/pages/Book.tsx
+++ src/pages/Book.tsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
+import { UserContext } from "../contexts/User";
+
 import BookMenu from "../components/BookMenu";
 import BookCard from "../components/BookCard";
 
@@ -16,6 +18,8 @@ interface IBook {
 
 const Book = () => {
 	const { id } = useParams();
+
+	const user = useContext(UserContext);
 
 	const [book, setBook] = useState<IBook | null>(null);
 	const [recommendations, setRecommendations] = useState([]);
@@ -45,7 +49,7 @@ const Book = () => {
 			{book &&
 			<>
 				<div className="container mx-auto mt-8">
-					<div className="inline-block float-right"><BookMenu book={book} /></div>
+					{user && <div className="inline-block float-right"><BookMenu book={book} /></div>}
 					<div className="bg-white shadow-md rounded-lg p-4">
 						<div className="flex justify-center">
 							<img src={book.image} className="h-48 w-32 object-cover" />
