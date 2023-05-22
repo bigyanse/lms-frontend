7f79f12292f7c9e98b851f8a244903df2f1236c2
bigyanse <contact.bigyanse@gmail.com>
2023-05-22 15:56:39 +0545

    `update`: small change

--- src/contexts/User.tsx
+++ src/contexts/User.tsx
@@ -5,6 +5,8 @@ interface User {
 	admin: boolean;
 };
 
+const BACKEND_BASE_URL = "https://lms-backend-v2qw.onrender.com";
+
 export const UserContext = createContext<User | null>(null);
 
 export const UserProvider = ({ children }: { children: ReactNode }) => {
@@ -12,7 +14,7 @@ export const UserProvider = ({ children }: { children: ReactNode }) => {
 
 	useEffect(() => {
 		const fetchUser = async () => {
-			const response = await fetch("http://localhost:5000/auth/me", { credentials: 'include' });
+			const response = await fetch(`${BACKEND_BASE_URL}/auth/me`, { credentials: 'include' });
 			const data = await response.json();
 			if(data.success) {
 				setUser(data.data.user);
